cec7494744ca816875c9eca11831f140bc3fffef
chibimayura <chibimayura@gmail.com>
2018-06-29 23:48:43 -0700

    add timer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,24 @@
+var startTimer;
+
+var timer = 30;
+
+var isGame = false;
+
 var quizOne = $(".typeOne");
+var start = $(".start");
+var timeID = $("#timer");
 
-// quizOne.hide();
+quizOne.hide();
 
-$(".start").hide();
+function countDownTimer(){
+	timer--;
+
+	timeID.text(timer);
+}
+
+$(".start-btn").on("click", function(){
+	quizOne.show();
+	start.hide();
+
+	startTimer = setInterval(countDownTimer, 1000);
+});
